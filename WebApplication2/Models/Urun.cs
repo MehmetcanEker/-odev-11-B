78c29934e5d4f145d1b0593e132a112f9ba3dd9d
MehmetcanEker <ekermehmetcan08@gmail.com>
2024-03-07 14:04:37 +0300

    Proje dosyası ekle.

---
+++ WebApplication2/Models/Urun.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; } // 1
+        public string Isım { get; set; } // Lenovo Ultrabook V4
+        public string Aciklama { get; set; } // Bu ürünün ağırlığı yaklaşık 2400 gramdır.
+        public string Kategori { get; set; } // Elektronik
+        public int Fiyat { get; set; } // 13999
+
+    }
+}
